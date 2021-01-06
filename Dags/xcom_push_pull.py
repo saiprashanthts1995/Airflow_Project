9b4c44ed23fc0c9b167b2250457a27f22f016ce6
Sai Prashanth T S <saiprashanthts@gmail.com>
2021-01-06 10:25:25 +0530

    xcom push and pull

---
+++ Dags/xcom_push_pull.py
@@ -0,0 +1,62 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.operators import BashOperator, DummyOperator, PythonOperator
+from airflow.utils.dates import days_ago
+from datetime import timedelta
+
+
+def push_function(ti):
+    """
+    Simple Python Function to push value
+    :return:
+    """
+    a = 1
+    b = 2
+    c= a + b
+    ti.xcom_push(key='c', value=c)
+    return a+b
+
+
+def receive_function(ti):
+    """
+    to receive the value from XCOM
+    :param ti: cross function element
+    :return:
+    """
+    output = ti.xcom_pull(key='c',task_ids=['Xcom_Push'])
+    print(output)
+
+
+dag_arguments = {
+    'Owner': 'Sai',
+    'depends_on_past': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG(
+    dag_id='Xcom_Push_Pull',
+    default_args=dag_arguments,
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=days_ago(2)
+)
+
+dummy_task = DummyOperator(
+    task_id='Start',
+    dag=dag
+)
+
+push_task = PythonOperator(
+    task_id='Xcom_Push',
+    python_callable=push_function,
+    dag=dag
+)
+
+pull_task = PythonOperator(
+    task_id='Xcom_Pull',
+    python_callable=receive_function,
+    dag=dag
+)
+
+dummy_task >> push_task >> pull_task
